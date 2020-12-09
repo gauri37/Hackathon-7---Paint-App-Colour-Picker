0505f9ca183adf5d6bc8da1edd5f38286727dddd
gauri38 <kumbhar.gauri@gmail.com>
2020-12-09 19:28:15 +0530

    alldone

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const ColourSelector = (props) => {
-  const { config, selectNextBackground } = props
+  const { config, selectNextBackground } = props;
   const { background } = config;
   return (
-    <button className={/* classname should come here */} onClick={() => selectNextBackground({background: background})}>
-      {/* label should come here */}
+    <button
+      className={`btn-${config.key}`}
+      onClick={() => {
+        selectNextBackground({ background: background });
+      }}
+    >
+      ColourSelector
     </button>
-  )
-}
+  );
+};
 export default ColourSelector;
